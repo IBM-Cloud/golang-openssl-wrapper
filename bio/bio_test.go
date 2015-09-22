002bf2720c8510266ffb9e4ba046224f83e1e95d
Colton Baker <github@netrefuge.net>
2015-09-22 13:21:08 -0400

    Add BIO_new, BIO_s_mem and BIO_s_file (with tests)

--- bio/bio_test.go
+++ bio/bio_test.go
@@ -10,6 +10,63 @@ import (
 
 var _ = Describe("Bio", func() {
 	Describe("Basic I/O", func() {
+		var (
+			b    BIO
+			text = "Some test data"
+		)
+
+		Context("Using a memory store", func() {
+			It("Should create a new bio using memory store", func() {
+				b = BIO_new(BIO_s_mem())
+				Expect(b).NotTo(BeNil())
+			})
+
+			It("Should be writable", func() {
+				r := BIO_puts(b, text)
+				Expect(r).NotTo(Equal(-2))
+			})
+
+			It("Should be readable", func() {
+				buf := make([]byte, len(text))
+				r := BIO_gets(b, buf, len(text)+1)
+				Expect(r).To(Equal(len(text)))
+				Expect(string(buf)).To(Equal(text))
+			})
+
+			It("Should free the memory store bio", func() {
+				Expect(BIO_free(b)).To(Equal(1))
+			})
+		})
+
+		Context("Using file storage", func() {
+			It("Should create a new bio using file storage", func() {
+				b = BIO_new(BIO_s_file())
+				Expect(b).NotTo(BeNil())
+			})
+
+			It("Should free the file storage bio", func() {
+				Expect(BIO_free(b)).To(Equal(1))
+			})
+
+			It("Should be writable with BIO_printf", func() {
+				b = BIO_new(BIO_s_file())
+				Expect(BIO_write_filename(b, "biotest.out")).To(Equal(1))
+				Expect(BIO_seek(b, 0)).To(BeEquivalentTo(0))
+				Expect(BIO_printf(b, text)).To(Equal(len(text)))
+				Expect(BIO_free(b)).To(Equal(1))
+			})
+
+			It("Should be readable", func() {
+				buf := make([]byte, len(text))
+				b = BIO_new(BIO_s_file())
+				Expect(BIO_read_filename(b, "biotest.out")).To(Equal(1))
+				Expect(BIO_seek(b, 0)).To(BeEquivalentTo(0))
+				Expect(BIO_gets(b, buf, len(text)+1)).To(Equal(len(text)))
+				Expect(string(buf)).To(Equal(text))
+				Expect(BIO_free(b)).To(Equal(1))
+			})
+		})
+
 		Context("Making a connection", func() {
 			It("Connects successfully", func() {
 				dest := "www.google.com:http"
@@ -35,7 +92,7 @@ var _ = Describe("Bio", func() {
 				/* Assumes only a single BIO in the chain... */
 				Expect(BIO_free(fbio)).To(Equal(1))
 			})
-	
+
 			It("Writes to the file, reads using native Go I/O", func() {
 				Expect(BIO_puts(fbio, text)).To(BeNumerically(">=", 1))
 				Expect(BIO_flush(fbio)).To(BeEquivalentTo(1))
@@ -54,7 +111,7 @@ var _ = Describe("Bio", func() {
 				Expect(BIO_seek(fbio, 0)).To(BeEquivalentTo(0))
 
 				rbuf := make([]byte, len(text))
-				l := BIO_gets(fbio, rbuf, len(text) +1)
+				l := BIO_gets(fbio, rbuf, len(text)+1)
 				/* Check that we've read enough bytes */
 				Expect(l).To(BeNumerically(">=", len(text)))
 
