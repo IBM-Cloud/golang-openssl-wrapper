21582dc8c3494a657b016f00e2cce0ace2fca577
Sandy Cash <lhcash@us.ibm.com>
2015-10-07 11:16:59 -0400

    Added tests for FIPS control over allowed digest methods

--- digest/digest_test.go
+++ digest/digest_test.go
@@ -1,6 +1,7 @@
 package digest_test
 
 import (
+	"github.com/ScarletTanager/openssl/crypto"
 	. "github.com/ScarletTanager/openssl/digest"
 
 	. "github.com/onsi/ginkgo"
@@ -22,6 +23,30 @@ var _ = Describe("Digest", func() {
 			EVP_MD_CTX_init(ctx)
 			Expect(EVP_MD_CTX_cleanup(ctx)).To(Equal(1))
 			Free_EVP_MD_CTX(ctx)
+		})
+	})
+
+	Context("Hashing binary data", func() {
+		Context("With FIPS mode enabled", func() {
+			var ctx EVP_MD_CTX
+
+			BeforeEach(func() {
+				crypto.FIPS_mode_set(1)
+				Expect(crypto.FIPS_mode()).To(Equal(1))
+				ctx = Malloc_EVP_MD_CTX()
+				Expect(ctx).NotTo(BeNil())
+				EVP_MD_CTX_init(ctx)
+			})
+
+			AfterEach(func() {
+				crypto.FIPS_mode_set(0)
+				Expect(crypto.FIPS_mode()).To(Equal(0))
+			})
+
+			It("Allows use of SHA* but disallows MD5", func() {
+				Expect(EVP_DigestInit_ex(ctx, EVP_md5(), SwigcptrStruct_SS_engine_st(0))).To(Equal(0))
+				Expect(EVP_DigestInit_ex(ctx, EVP_sha256(), SwigcptrStruct_SS_engine_st(0))).To(Equal(1))
+			})
 		})
 	})
 })
